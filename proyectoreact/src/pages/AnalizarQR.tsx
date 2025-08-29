c7709e2f5dc7bf0bc21ae463f4acb97eeaff67ab
Pausrq27 <masanchezro@est.utn.ac.cr>
2025-08-29 17:02:29 -0600

    sd

--- proyectoreact/src/pages/AnalizarQR.tsx
+++
@@ -1,8 +0,0 @@
-export default function AnalizarQR() {
-  return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>🔍 Página de Analizar QR</h1>
-      <p>Solo accesible para Cajero</p>
-    </div>
-  );
-}
--- proyectoreact/src/pages/RegistrosEntrega.tsx
+++
@@ -1,8 +0,0 @@
-export default function RegistrosEntrega() {
-  return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>📦 Página de Registros de Entrega</h1>
-      <p>Solo accesible para Agente Manual</p>
-    </div>
-  );
-}
